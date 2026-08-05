f64a980f74be52041e748123d7e9feb4b4785c55
SaiPranab <saipranabpatra1234@gmail.com>
2026-08-05 09:12:33 +0530

    rest imp method of array

--- 04 javascript/lec 9 - Array/array imp methods.js
+++ 04 javascript/lec 9 - Array/array imp methods.js
@@ -27,55 +27,84 @@ let numbers = [10, 20, 30, 40, 50]
 
 
 
-// =============================== map function
-const numbersIncreasedByTwo = numbers.map((value, idx, arr) => {
-  console.log("value is", value, "at idx", idx, "of array", arr)
-  return value + 2
-})
-console.log('numbersIncreasedByTwo', numbersIncreasedByTwo)
-
-const squareNumbers = numbers.map(value => value * value)
-console.log('squareNumbers', squareNumbers)
-
-const users = [
-  {
-    id: 101,
-    name: 'Sambit Parida',
-    salary: 100000
-  },
-  {
-    id: 102,
-    name: 'Snigdha Mohanty',
-    salary: 110000
-  },
-  {
-    id: 103,
-    name: 'Aiswarya Ayeshkant',
-    salary: 120000
-  },
-  {
-    id: 104,
-    name: 'Sinumayee Prusty',
-    salary: 130000
-  },
-]
-
-// const modfiedUsers = users.map((value) => {
-//   console.log("Value is", value)
-
-//   return {
-//     id: value.id,
-//     firstname: value.name.split(" ")[0],
-//     bonus: value.salary * .10
-//   }
+// // =============================== map function
+// const numbersIncreasedByTwo = numbers.map((value, idx, arr) => {
+//   console.log("value is", value, "at idx", idx, "of array", arr)
+//   return value + 2
 // })
+// console.log('numbersIncreasedByTwo', numbersIncreasedByTwo)
 
-const modfiedUsers = users.map(user => ({
-  id: user.id,
-  firstname: user.name.split(" ")[0],
-  bonus: user.salary * .10
-}))
-console.log("// modfied users", modfiedUsers)
+// const squareNumbers = numbers.map(value => value * value)
+// console.log('squareNumbers', squareNumbers)
+
+// const users = [
+//   {
+//     id: 101,
+//     name: 'Sambit Parida',
+//     salary: 100000
+//   },
+//   {
+//     id: 102,
+//     name: 'Snigdha Mohanty',
+//     salary: 110000
+//   },
+//   {
+//     id: 103,
+//     name: 'Aiswarya Ayeshkant',
+//     salary: 120000
+//   },
+//   {
+//     id: 104,
+//     name: 'Sinumayee Prusty',
+//     salary: 130000
+//   },
+// ]
+
+// // const modfiedUsers = users.map((value) => {
+// //   console.log("Value is", value)
+
+// //   return {
+// //     id: value.id,
+// //     firstname: value.name.split(" ")[0],
+// //     bonus: value.salary * .10
+// //   }
+// // })
+
+// const modfiedUsers = users.map(user => ({
+//   id: user.id,
+//   firstname: user.name.split(" ")[0],
+//   bonus: user.salary * .10
+// }))
+// console.log("// modfied users", modfiedUsers)
+
 
 // ========================== filter function
-numbers = [10, 11, 20, 21, 30, 31]
+// numbers = [10, 11, 20, 21, 30, 31]
+
+// // const divisibleByTen = numbers.filter((value, idx, arr) => {
+// //   console.log("value is", value, "at idx", idx, "of array", arr)
+
+// //   return value % 10 == 0
+// // })
+// // console.log("divisible By ten", divisibleByTen)
+// // divisibleByTen.forEach(value => console.log(value))
+
+// numbers.filter()
+
+// =========================== some()
+// numbers = [10, 20, 30, 40, 41, 51]
+// const isAnyOddPresent = numbers.some(num => num % 2 == 1)
+// console.log("///// is Any Odd Present", isAnyOddPresent)
+
+// const isEveryMemberAreEven = numbers.every(num => num % 2 == 0)
+// console.log("//// is every members are even", isEveryMemberAreEven)
+
+
+// =========================== find(), findIndex()
+numbers = [10, 20, 30, 40,  51]
+
+const number = numbers.find(num => num == 41)
+console.log("is 41 found", number)
+
+const numberIdx = numbers.findIndex(num => num == 41)
+console.log("is 41 found", numberIdx)
