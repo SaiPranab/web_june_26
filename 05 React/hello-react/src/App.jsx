d5acc22becb325520807ad928357e2e3b88d1283
SaiPranab <saipranabpatra1234@gmail.com>
2026-09-03 09:22:10 +0530

    Props

--- 05 React/hello-react/src/App.jsx
+++ 05 React/hello-react/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Fragment } from 'react'
 import './App.css'
+import Sai from './Sai'
+import Item from "./Product"
 
 function App() {
   const a = 10
@@ -44,6 +46,13 @@ function App() {
       <p>a is : {a}</p>
       <p>b is : {b}</p>
       <p>a + b is : { a + b }</p>
+
+      {/* { Sai() }
+      <Sai></Sai> */}
+      <Sai />
+
+      <Item name="Watch" price="2500" description="This is a watch" brand="rolex" />
+      <Item  price="30000.99" brand="Samsung" />
     </>
   )
 }
---
+++ 05 React/hello-react/src/Sai.jsx
@@ -0,0 +1,7 @@
+function Sai() {
+  return (
+    <h3>How's it going ?</h3>
+  )
+}
+
+export default Sai;
