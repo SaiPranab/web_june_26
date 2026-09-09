b8a5ff57db528c661d3e46b1378958530690c304
SaiPranab <saipranabpatra1234@gmail.com>
2026-09-09 08:54:46 +0530

    find country using searchbar

--- 05 React/rest-countries/src/components/CountriesList.jsx
+++ 05 React/rest-countries/src/components/CountriesList.jsx
@@ -1,22 +1,29 @@
+import { useState } from "react";
 import countriesData from "../countriesData";
 import CountryCard from "./CountryCard";
 
-export default function CountriesList() {
+export default function CountriesList({ query }) {
+  const filteredCountries = countriesData.filter(country => 
+              country.name.common.toLowerCase().includes(query.toLowerCase()))
 
   return (
-    <div className="countries-container">
-      {
-        countriesData.map((country, idx) => (
-          <CountryCard 
-            key={idx}
-            flag={country.flags.svg}
-            name={country.name.common}
-            population={country.population}
-            capital={country.capital}
-            region={country.region}
-          />
-        ))
-      }
-    </div>
+    <>
+      <div className="countries-container">
+        {
+          filteredCountries.length != 0 ? 
+            (filteredCountries.map((country, idx) => (
+              <CountryCard 
+                key={idx}
+                flag={country.flags.svg}
+                name={country.name.common}
+                population={country.population}
+                capital={country.capital}
+                region={country.region}
+              />)))
+            :
+              <p>Unable to find Country with name:- {query}</p>
+        }
+      </div>
+    </>
   )
 }
